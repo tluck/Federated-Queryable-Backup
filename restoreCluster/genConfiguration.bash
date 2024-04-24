5ef90eec131d817a8f2b586937ca8c03138c0c80
tluck <tjluckenbach@gmail.com>
2024-04-24 13:57:25 -0700

    2024.04.24
    * simplified the FederationDB config by using a wildcard for the collection names

--- restoreCluster/genConfiguration.bash
+++ restoreCluster/genConfiguration.bash
@@ -2,17 +2,8 @@
 
 dbName="${1:-test}"
 source init.conf
-tenantName="federatedDB-${dbName//_/-}"
 
-list=( $( getCollections.bash ${dbName} ) )
-len=${#list[@]}
-
-if [[ $len < 1 ]]
-then
-    printf "* * * error: No DB match $dbName\n"
-    exit 1
-fi
-
+# using a wildcard for the collection names
 cat <<EOF1
 {
   "name": "${tenantName}",
@@ -22,30 +13,9 @@ cat <<EOF1
 EOF1
   printf "\t{\"collections\": [\n"
 
-n=0
-max=$((len-1))
-while [ $n -lt $max ]
-do
-  name=${list[$n]}
-  # db=${name%%.*}
-  collection=${name#*.}
-  printf "\t\t{\"name\": \"${collection}\",\n"
-  printf "\t\t\"dataSources\": [\n"
-  printf "\t\t\t{\"collection\": \"${collection}\",\n" 
-  printf "\t\t\t\"database\":   \"${dbName}\",\n" 
-  printf "\t\t\t\"storeName\":  \"${restoreCluster}\"\n" 
-  printf "\t\t\t}]\n" 
-  printf "\t\t},\n" 
-
-  n=$((n+1))
-done
-
-name=${list[$n]}
-# db=${name%%.*}
-collection=${name#*.}
-printf "\t\t{\"name\": \"${collection}\",\n"
+printf "\t\t{\"name\": \"*\",\n"
 printf "\t\t\"dataSources\": [\n"
-printf "\t\t\t{\"collection\": \"${collection}\",\n" 
+printf "\t\t\t{\n" 
 printf "\t\t\t\"database\":   \"${dbName}\",\n" 
 printf "\t\t\t\"storeName\":  \"${restoreCluster}\"\n" 
 printf "\t\t\t}]\n" 
